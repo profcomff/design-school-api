85ad25931fbd433ecf0a443d7e4b8732f5d03883
semen603089 <semen603089@mail.ru>
2022-11-06 10:30:21 +0300

    spam fix

--- design_bot/routes/spam.py
+++ design_bot/routes/spam.py
@@ -8,16 +8,16 @@ from design_bot.models.db import SpamBeforeRegistration
 from .models.models import SpamPost, SpamGet
 from ..methods import auth
 
-spam = APIRouter(prefix="/sign-up", tags=["Spam"])
+spam = APIRouter(prefix="/spam", tags=["Spam"])
 
 
-@spam.post("/spam", response_model=None)
+@spam.post("/", response_model=None)
 async def add_user_to_spam(spam_post: SpamPost) -> PlainTextResponse:
     db.session.add(SpamBeforeRegistration(**spam_post.dict()))
     db.session.flush()
     return PlainTextResponse(status_code=201, content="User added tp spam list")
 
 
-@spam.get("/spam", response_model=list[SpamGet])
+@spam.get("/", response_model=list[SpamGet])
 async def get_spam_list(_: auth.User = Depends(auth.get_current_user)) -> list[SpamGet]:
     return parse_obj_as(list[SpamGet], db.session.query(SpamBeforeRegistration).all())
